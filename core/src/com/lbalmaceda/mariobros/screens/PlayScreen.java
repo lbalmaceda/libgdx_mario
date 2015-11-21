efee9a35f7c36172a2cf891b3bf8bc6f487dbbe0
lbalmaceda <balmacedaluciano@gmail.com>
2015-11-21 00:05:15 -0300

    add tiled map for level 1

--- core/src/com/lbalmaceda/mariobros/screens/PlayScreen.java
+++ core/src/com/lbalmaceda/mariobros/screens/PlayScreen.java
@@ -4,6 +4,9 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.lbalmaceda.mariobros.MarioBros;
 import com.lbalmaceda.mariobros.scenes.Hud;
@@ -16,24 +19,47 @@ public class PlayScreen implements Screen {
     private final Hud hud;
     private OrthographicCamera gameCam;
     private final FitViewport gamePort;
+    private TmxMapLoader mapLoader;
+    private TiledMap map;
+    private OrthogonalTiledMapRenderer renderer;
 
     public PlayScreen(MarioBros game) {
         this.game = game;
         gameCam = new OrthographicCamera();
         gamePort = new FitViewport(MarioBros.V_WIDTH, MarioBros.V_HEIGHT, gameCam);
         hud = new Hud(game.batch);
+        mapLoader = new TmxMapLoader();
+        map = mapLoader.load("level1.tmx");
+        renderer = new OrthogonalTiledMapRenderer(map);
+
+        gameCam.position.set(gamePort.getWorldWidth() / 2, gamePort.getWorldHeight() / 2, 0);
     }
 
     @Override
     public void show() {
+    }
 
+    public void update(float dt) {
+        handleInput(dt);
+
+        gameCam.update();
+        renderer.setView(gameCam);
+    }
+
+    public void handleInput(float dt) {
+        if (Gdx.input.isTouched()) {
+            gameCam.position.x += 100 * dt;
+        }
     }
 
     @Override
     public void render(float delta) {
+        update(delta);
+
         Gdx.gl.glClearColor(1, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         game.batch.setProjectionMatrix(hud.stage.getCamera().combined);
+        renderer.render();
         hud.stage.draw();
     }
 
